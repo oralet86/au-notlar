e840679942896cc5e7f1170fa035fbfd08a133a3
oralet86 <merocks86@gmail.com>
2025-01-12 21:06:37 +0300

    Moved code into classes

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,16 +2,28 @@ from selenium import webdriver
 
 OBS_LOGIN_URL = "https://obs.ankara.edu.tr/Account/Login"
 
-browser = webdriver.Firefox()
+class ScraperManager:
+  instances = []
 
-browser.get(OBS_LOGIN_URL)
+class OBSScraper:
+  browser: webdriver.Firefox = None
+  state = "init"
 
-username_input = browser.find_element("css selector", "#OtherUsername")
-password_input = browser.find_element("css selector", "#OtherPassword")
-captcha_input = browser.find_element("css selector", "#Captcha")
-login_button = browser.find_element("css selector", "#btnSend")
+  def __init__(self):
+    browser = webdriver.Firefox()
+    browser.get(OBS_LOGIN_URL)
+    self.browser = browser
+    
+  def getLoginElements(self):
+    username_input = self.browser.find_element("css selector", "#OtherUsername")
+    password_input = self.browser.find_element("css selector", "#OtherPassword")
+    captcha_photo = self.browser.find_element("xpath", "/html/body/div[4]/form/div[4]/img")
+    captcha_input = self.browser.find_element("css selector", "#Captcha")
+    login_button = self.browser.find_element("css selector", "#btnSend")
+    
+    return {"username": username_input, "password": password_input, "captcha_photo": captcha_photo, "captcha": captcha_input, "login": login_button}
 
-with open('filename.png', 'wb') as file:
-  file.write(browser.find_element("xpath", "/html/body/div[4]/form/div[4]/img").screenshot_as_png)
-
-browser.quit()
+  browser.quit()
+  
+if __name__ == "__main__":
+  x = OBSScraper()
