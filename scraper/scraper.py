dcd61fe9ad8b778cb6d8386ede198ee5a6ca6e58
oralet86 <merocks86@gmail.com>
2025-01-13 12:37:04 +0300

    Implemented determineState helper functions and attemptLogin function

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from typing import Literal
+from PIL import Image
+import numpy as np
+import io
+from ocr import solver as s
 
 OBS_LOGIN_URL = "https://obs.ankara.edu.tr/Account/Login"
 
@@ -19,7 +23,7 @@ class OBSScraper:
         while True:
             match self.state:
                 case "init":
-                    ...
+                    self.attemptLogin()
                 case "mainmenu":
                     ...
                 case "examresults":
@@ -28,7 +32,6 @@ class OBSScraper:
                     print("Confused state. Quitting and restarting the browser.")
                     self.quit()
                     self.start()
-                    break
 
     def determineState(self):
         if self.isInLogin():
@@ -49,11 +52,38 @@ class OBSScraper:
         except Exception:
             return False
 
-    def isInForm(self): ...
+    def isInForm(self):
+        try:
+            self.browser.find_element("xpath", "/html/body/div[16]")
+            return True
+        except Exception:
+            return False
 
-    def isInMainmenu(self): ...
+    def isInMainmenu(self):
+        try:
+            self.browser.find_element("css selector", "#column")
+            return True
+        except Exception:
+            return False
 
-    def isInExamResults(self): ...
+    def isInExamResults(self):
+        try:
+            self.browser.find_element("css selector", "#confirmationReport-list")
+            return True
+        except Exception:
+            return False
+
+    def attemptLogin(self):
+        elements = self.getLoginElements()
+        elements["username"].send_keys()
+        elements["password"].send_keys()
+        image = np.array(
+            Image.open(io.BytesIO(elements["captcha_photo"].screenshot_as_png))
+        )
+        solver = s.CaptchaSolver(image)
+        result = solver.solve_captcha()
+        elements["captcha"].send_keys(str(result))
+        elements["login"].click()
 
     def getLoginElements(self):
         username_input = self.browser.find_element("css selector", "#OtherUsername")
@@ -82,6 +112,9 @@ class OBSScraper:
         browser.get(OBS_LOGIN_URL)
         self.browser = browser
 
+    def __del__(self):
+        self.quit()
+
 
 class CaptchaScraper(OBSScraper):
     def __init__(self):
@@ -92,3 +125,4 @@ class CaptchaScraper(OBSScraper):
 
 if __name__ == "__main__":
     x = OBSScraper()
+    x.attemptLogin()
