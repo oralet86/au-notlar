5e1542eb90d7aca69de96727dbd3b7979abaaa45
oralet86 <merocks86@gmail.com>
2025-01-14 11:36:08 +0300

    Implemented load_accounts() and added *.db to .gitignore

--- manager/manager.py
+++ manager/manager.py
@@ -1,10 +1,11 @@
 import sqlite3
-from scraper.scraper import OBSScraper
+import json
 
 
 class Manager:
-    _instance: "Manager" = ...
-    conn = sqlite3.connect("notlar.db")
+    _instance: "Manager" = None
+    accounts: list[dict] = None
+    conn = sqlite3.connect("manager/notlar.db")
     cursor = conn.cursor()
 
     def __new__(cls):
@@ -12,4 +13,13 @@ class Manager:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self): ...
+    def __init__(self):
+        self.load_accounts()
+
+    def load_accounts(self):
+        with open("manager/accounts.json", "r", encoding="utf-8") as file:
+            self.accounts = json.load(file)
+
+
+if __name__ == "__main__":
+    Manager()
