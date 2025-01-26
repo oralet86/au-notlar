607842d4156b4167717bf2c474e784e4429ccfb1
oralet86 <merocks86@gmail.com>
2025-01-26 12:55:00 +0300

    Added SQL database to store extracted exam results

--- manager/manager.py
+++ manager/manager.py
@@ -3,6 +3,7 @@ import threading
 import time
 from scraper.scraper import OBSScraper
 from logging_config import logger
+import sqlite3
 
 
 class Manager:
@@ -11,6 +12,8 @@ class Manager:
     scrapers: list[OBSScraper] = []
     interval: int = 60
     lock = threading.Lock()
+    conn = sqlite3.connect("results.db", check_same_thread=False)
+    cursor = conn.cursor()
 
     def __new__(cls):
         if cls._instance is None:
@@ -19,10 +22,10 @@ class Manager:
         return cls._instance
 
     def __init__(self):
+        self.initializeDatabase()
         self.loadAccounts()
         self.initializeScrapers()
         self.startScrapers()
-        self.runScrapers()
 
     def loadAccounts(self):
         logger.info("Loading accounts.")
@@ -50,6 +53,9 @@ class Manager:
                     scraper.refresh()
                     scraper.navigateSite()
                     scraper.extractResults()
+                    if scraper.results is not None:
+                        with self.lock:
+                            self.upsert_data(scraper.label, scraper.results)
 
                 elapsed_time = time.time() - start_time
                 logger.info(f"Completed execution in: {elapsed_time:.2f} seconds")
@@ -61,6 +67,124 @@ class Manager:
         thread = threading.Thread(target=scrape)
         thread.start()
 
+    @classmethod
+    def initializeDatabase(cls):
+        cls.cursor.execute(
+            """
+        CREATE TABLE IF NOT EXISTS Departments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )"""
+        )
+
+        cls.cursor.execute(
+            """
+        CREATE TABLE IF NOT EXISTS Lectures (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            department_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            FOREIGN KEY (department_id) REFERENCES Departments(id)
+        )"""
+        )
+
+        cls.cursor.execute(
+            """
+        CREATE TABLE IF NOT EXISTS Exams (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            lecture_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            percentage TEXT NOT NULL,
+            date TEXT NOT NULL,
+            FOREIGN KEY (lecture_id) REFERENCES Lectures(id)
+        )"""
+        )
+
+        cls.cursor.execute(
+            """
+        CREATE TABLE IF NOT EXISTS Notifications (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            department_id INTEGER NOT NULL,
+            nickname TEXT NOT NULL,
+            FOREIGN KEY (department_id) REFERENCES Departments(id)
+        )"""
+        )
+
+        cls.conn.commit()
+        return
+
+    @classmethod
+    def upsert_data(cls, department_name, lecture_data):
+        # Ensure department exists
+        cls.cursor.execute(
+            "SELECT id FROM Departments WHERE name = ?", (department_name,)
+        )
+        department_row = cls.cursor.fetchone()
+        if department_row is not None:
+            department_id = department_row[0]
+        else:
+            logger.info(
+                f'No value found for "{department_name}" in the Departments table. Creating one.'
+            )
+            cls.cursor.execute(
+                "INSERT INTO Departments (name) VALUES (?)", (department_name,)
+            )
+            department_id = cls.cursor.lastrowid
+
+        for lecture in lecture_data:
+            # Ensure lecture exists
+            cls.cursor.execute(
+                "SELECT id FROM Lectures WHERE name = ? AND department_id = ?",
+                (lecture["name"], department_id),
+            )
+            lecture_row = cls.cursor.fetchone()
+            if lecture_row is not None:
+                lecture_id = lecture_row[0]
+            else:
+                logger.info(
+                    f'No value found for "{lecture["name"]}" in the Lectures table. Creating one.'
+                )
+                cls.cursor.execute(
+                    "INSERT INTO Lectures (department_id, name) VALUES (?, ?)",
+                    (department_id, lecture["name"]),
+                )
+                lecture_id = cls.cursor.lastrowid
+
+            for exam in lecture["exams"]:
+                # Check for an exam result with identical values
+                cls.cursor.execute(
+                    """
+                    SELECT id FROM Exams
+                    WHERE lecture_id = ? AND name = ? AND percentage = ? AND date = ?
+                """,
+                    (lecture_id, exam["name"], exam["percentage"], exam["date"]),
+                )
+                exam_row = cls.cursor.fetchone()
+
+                if exam_row is None:
+                    # If none were found, that means new exam results were entered
+                    cls.cursor.execute(
+                        "DELETE FROM Exams WHERE lecture_id = ? AND name = ?",
+                        (lecture_id, exam["name"]),
+                    )
+                    cls.cursor.execute(
+                        """
+                        INSERT INTO Exams (lecture_id, name, percentage, date)
+                        VALUES (?, ?, ?, ?)
+                    """,
+                        (lecture_id, exam["name"], exam["percentage"], exam["date"]),
+                    )
+                    logger.info(
+                        f'New exam results found for "{lecture["name"]} / {exam["name"]}". Overwriting old ones.'
+                    )
+                else:
+                    ...
+                    # This line cluttered up the logs too much.
+                    # logger.info(f'Exam results of "{lecture["name"]} / {exam["name"]}" match the ones at the tables.')
+
+        logger.info("Finished database update.")
+        cls.conn.commit()
+
 
 if __name__ == "__main__":
     x = Manager()
+    x.runScrapers()
