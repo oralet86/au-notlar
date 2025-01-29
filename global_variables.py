496df3435da9f34485240b431308ff59858bac86
oralet86 <merocks86@gmail.com>
2025-01-29 18:08:40 +0300

    Added thread name to logs

--- global_variables.py
+++ global_variables.py
@@ -1,23 +1,24 @@
 import logging
 import time
+import os
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
 
 execution_time = time.strftime("%Y-%m-%d_%H-%M-%S")
 log_filename = f"logs/{execution_time}.log"
 
-with open(log_filename, "w"):
-    ...
-
-logging.basicConfig(
-    level=logging.INFO,
-    filename=log_filename,
-    encoding="utf-8",
-    filemode="a",
-    format="{asctime} - {levelname} - {message}",
-    style="{",
+log_formatter = logging.Formatter(
+    fmt="%(asctime)s - %(levelname)s - [%(threadName)s]: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 logger = logging.getLogger("main_logger")
+
+file_handler = logging.FileHandler(log_filename, encoding="utf-8")
+file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 logger.info("Booting up.")
 
